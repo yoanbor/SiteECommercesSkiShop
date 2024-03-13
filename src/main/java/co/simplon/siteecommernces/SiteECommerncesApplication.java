0707479ac0e0667a22ff51ab4d63eeff5565f579
Ylorklan <yoanbor@icloud.com>
2024-03-13 10:32:33 +0100

    Environment develop

---
+++ src/main/java/co/simplon/siteecommernces/SiteECommerncesApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.siteecommernces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteECommerncesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteECommerncesApplication.class, args);
+    }
+
+}
