11fbcb6282b6e8d7e871f6480286e2f960e44571
Ylorklan <yoanbor@icloud.com>
2024-03-16 10:50:58 +0100

    fix(controller): Mise en place du CrossOrigin

--- src/main/java/co/simplon/site/presentation/controller/product/ReadProductsController.java
+++ src/main/java/co/simplon/site/presentation/controller/product/ReadProductsController.java
@@ -3,12 +3,14 @@ package co.simplon.site.presentation.controller.product;
 import co.simplon.site.business.dto.ProductDTO;
 import co.simplon.site.business.service.product.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ReadProductsController {
     private final IProductService productService;
     @Autowired
